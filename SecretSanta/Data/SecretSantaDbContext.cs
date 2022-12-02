08acac4d2c16e32e01662799770fd41aba9b16b8
Daria Łuniewska <luniewska.d@gmail.com>
2022-12-02 20:26:20 +0100

    installed EF and Npgsql nugets, created dbCOntesx, scafolded new controllers and created initial migration

---
+++ SecretSanta/Data/SecretSantaDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecretSanta.Models;
+
+namespace SecretSanta.Data
+{
+    public class SecretSantaDbContext : DbContext
+    {
+        public SecretSantaDbContext(DbContextOptions<SecretSantaDbContext> options) :
+            base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gift>(entity =>
+            {
+                entity.Property(g => g.ID).ValueGeneratedOnAdd();
+
+            });
+
+            modelBuilder.Entity<Group>(entity =>
+            {
+                entity.Property(g => g.ID).ValueGeneratedOnAdd();
+
+            });
+        }
+
+        public DbSet<Gift> Gifts => Set<Gift>();
+        public DbSet<Group> Groups => Set<Group>();
+    }
+}
--- SecretSanta/Program.cs
+++ SecretSanta/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SecretSanta.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SecretSantaDbContext>(
+        o => o.UseNpgsql(builder.Configuration.GetConnectionString("SecretSantaDb"))
+        );
 
 var app = builder.Build();
 
